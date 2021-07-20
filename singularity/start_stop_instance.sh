13d8e15791cbd571ebe29ea407f800117d3f5f22
Brian D. Klahn <briandk@virginia.edu>
2021-07-20 11:31:38 -0400

    Try to make scipt more natural/clear to understand/use.

--- singularity/start_stop_instance.sh
+++ singularity/start_stop_instance.sh
@@ -5,16 +5,34 @@
 
 INSTANCE_NAME=cps_dis_lib_app
 
-if [ "$1" = "stop" ]
+print_usage() {
+    printf "
+    Start, or stop a COPASI Disease Library web application
+    as a Singularity service instance.
+    (See https://singularity.hpcng.org/user-docs/3.7/running_services.html)
+
+    Usage: $0 [start|stop]
+
+    With no arguments, or 'start', start the image instance.
+    'stop' will . . . well . . . you know . . .
+
+"
+}
+
+if [ -z $1 ] || [ "$1" = "start" ]
 then
-    ACTION=stopping
-    singularity instance stop $INSTANCE_NAME
-else
     ACTION=starting
     singularity instance start --writable-tmpfs COPASI_Disease_Lib_Shiny.sif $INSTANCE_NAME
 
     echo still need to make the image report that it is serving the app on . . .
     echo "localhost:4285"
+elif [ "$1" = "stop" ]
+then
+    ACTION=stopping
+    singularity instance stop $INSTANCE_NAME
+else
+    print_usage
+    exit 1
 fi
 
 printf "$ACTION $INSTANCE_NAME singularity instance\n"
